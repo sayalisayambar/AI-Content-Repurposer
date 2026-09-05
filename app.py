383193b1086184f725aececf6764c5772c8286b2
Sayali Sayambar <sayalisayambar89@gmail.com>
2026-09-05 08:13:07 +0530

    Update model version in API request

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def ask_ai(prompt):
             "Content-Type": "application/json"
         },
         json={
-            "model": "llama-3.1-8b-instant",
+           "model": "llama-3.3-70b-versatile",
             "messages": [
                 {
                     "role": "system",
